acb118e2ef4ae7c4ad227fe57de9531a0c9d5478
Kwon <wingseter@mju.ac.kr>
2020-09-07 13:22:28 +0900

    텍스트 입력 상자 생성

--- menus/online.py
+++ menus/online.py
@@ -1,5 +1,6 @@
 import pygame
 from tools.utils import emptyRoundRect
+from tools.textBox import textBox
 from loader import ONLINEMENU as ONLINE, FONT
 
 def showScreen(win):
@@ -9,23 +10,34 @@ def showScreen(win):
     win.blit(ONLINE.HEAD, (30, -20))
 
     for cnt, i in enumerate(ONLINE.TEXT):
-        win.blit(i, (40, 100 + cnt * 18))
-    
-    
+        win.blit(i, (40, 100 + cnt * 18))    
     
 
 def main(win):
     clock = pygame.time.Clock()
-    print("clicked")
+    pg = True
 
+    serverInput = textBox(FONT, (0, 0, 0), (300, 450, 200, 35))
     while True:
         clock.tick(24)
         showScreen(win)
 
+        if pg:
+            pygame.draw.rect(win, (255, 255, 255), (298, 448, 204, 39))
+            serverInput.draw(win)
+
         for event in pygame.event.get():
+            if pg:
+                serverInput.push(event)
+
             if event.type == pygame.QUIT:
                 return
+
             elif event.type == pygame.MOUSEBUTTONDOWN:
                 x, y = event.pos
+                if pg:
+                    pass
+                else:
+                    pass
 
         pygame.display.update()
