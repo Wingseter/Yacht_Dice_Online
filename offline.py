98ae2b546ca3008f339d5caa4554121b482710a7
Taewoongjung <70272679+Taewoongjung@users.noreply.github.com>
2020-09-22 14:28:31 +0900

    offline (version 2)
    UI

--- offline.py
+++ offline.py
@@ -1,69 +1,83 @@
 import pygame
-import menus
-import os
-from loader import MAIN
+import Countdown
 
 pygame.init()
+
+ourScreen = pygame.display.set_mode((1200, 750))
+pygame.display.set_caption('Yacht_Dice')
+
 clock = pygame.time.Clock()
 
-win = pygame.display.set_mode((1200, 750), 0, 32)
+finish = False
+colorBlue = True
 
-win.fill((255, 209, 155))
-    clock.tick(30)
+clock = pygame.time.Clock()
+counter, text = 30, '30'.rjust(3)
+pygame.time.set_timer(pygame.USEREVENT, 1000)
+font = pygame.font.SysFont('Consolas', 30)
 
-    x, y = pygame.mouse.get_pos()
-    if offln[0] < x < sum(offln[::2]) and offln[1] < y < sum(offln[1::2]):
-        win.blit(MAIN.OFFLINE_H, offln[:2])
-    elif onln[0] < x < sum(onln[::2]) and onln[1] < y < sum(onln[1::2]):
-        win.blit(MAIN.ONLINE_H, onln[:2])
-    elif opt[0] < x < sum(opt[::2]) and opt[1] < y < sum(opt[1::2]):
-        win.blit(MAIN.OPTION_H, opt[:2])
-    elif qit[0] < x < sum(qit[::2]) and qit[1] < y < sum(qit[1::2]):
-        win.blit(MAIN.QUIT_H, qit[:2])
+#game loop(로직 생성)
+while not finish:
+    for event in pygame.event.get(): #발생한 이벤트 리스트 가져오기
+        if event.type == pygame.QUIT: #파이 게임이 끝났으면
+            finish = True 
 
-while True:
-    def draw_button(button, screen):
+    ourScreen.fill((250,209,155))
+  
+    color1 = (255,255,255)
+    color2 = (250,222,255)
+    pygame.draw.rect(ourScreen, color1, pygame.Rect(30,30,120,120))
+    pygame.draw.rect(ourScreen, color2, pygame.Rect(1000,20,77,70))
+    # update the screen
 
-     pygame.draw.rect(screen, button['color'], button['rect'])
-     screen.blit(button['text'], button['text rect'])
+#    pygame.display.flip()
+    for e in pygame.event.get():
+        if e.type == pygame.USEREVENT: 
+          counter -= 1
+          text = str(counter).rjust(3) if counter > 0 else 'boom!'
+        if e.type == pygame.QUIT: break
+        else:
 
-    def create_button(x, y, w, h, text, callback, ):
+          ourScreen.blit(font.render(text, True, (0, 0, 0)), (1000,20,30,40))
+          pygame.display.flip()
+          clock.tick(60)
+          continue
+          break
 
-          
-    text_surf = FONT.render(text, True, WHITE)
-    button_rect = pygame.Rect(x, y, w, h)
-    text_rect = text_surf.get_rect(center=button_rect.center)
-    button = {
-        'rect': button_rect,
-        'text': text_surf,
-        'text rect': text_rect,
-        'color': INACTIVE_COLOR,
-        'callback': callback,
-        }
-    return button
+"""
+    if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE: #스페이스바 누르면 색
+        colorBlue = not colorBlue
+    pressed = pygame.key.get_pressed()
+    if pressed[pygame.K_UP]: y-=3
+    if pressed[pygame.K_DOWN]: y+=3
+    if pressed[pygame.K_LEFT]: x-=3
+    if pressed[pygame.K_RIGHT]: x+=3
 
+    if colorBlue: color = (0,128,255) #파란색으로 변경
+    else: color = (255,255,255) #흰색으로 변경
 
-     def printing_dice():
-        nonlocal dice
-        nonlocal round_count
-        dice = None
-        dice = dice_func.dice()
-        round_count += 1
+    pygame.draw.rect(ourScreen, color, pygame.Rect(x,y,60,60))  #ourScreen 에 사각형 그리고, 색 지정해주기, 좌표와 크기 지정
+    pygame.display.flip() #display 업데이트 (flip()또는 update())
 
-
-        pygame.draw.rect(screen, BLACK, [10, 10, 880, 580], 6)
-
-        pygame.draw.line(screen, BLACK, [220, 10], [220, 590], 3)
-        pygame.draw.line(screen, BLACK, [440, 10], [440, 590], 3)
-        pygame.draw.line(screen, BLACK, [660, 10], [660, 590], 3)
-
-        pygame.draw.line(screen, BLACK, [10, 92],  [890, 92],  3)
-        pygame.draw.line(screen, BLACK, [10, 174], [890, 174], 3)
-        pygame.draw.line(screen, BLACK, [10, 256], [890, 256], 3)
-        pygame.draw.line(screen, BLACK, [10, 338], [890, 338], 3)
-        pygame.draw.line(screen, BLACK, [10, 420], [890, 420], 3)
-        pygame.draw.line(screen, BLACK, [10, 502], [890, 502], 3)
+    clock.tick(60)
+ """
 
 
-        pygame.display.update()
-        clock.tick(30)
+
+"""
+def setup():
+    frameRate(60)
+    size(500, 500)
+    noLoop()
+
+def draw(self):
+  background('#777777')
+
+  noStroke()
+  fill('#ffffff')
+  rectMode(CENTER)
+  rect(250, 250, 100, 100)
+
+  fill('#333333')
+  ecllipse(250,250,20,20)
+"""
