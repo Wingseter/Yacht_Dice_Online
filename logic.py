c12039003193b009064e7372142208fe1efc722f
문정환 <70272622+bazzi2548@users.noreply.github.com>
2020-09-28 23:20:48 +0900

    Add files via upload

--- logic.py
+++ logic.py
@@ -8,7 +8,7 @@ def HighestRepeated(dice, minRepeats):
 
 def OfAKind(dice, n):
     repeat_score = HighestRepeated(dice,n)
-    remainder = [x for x in dice.x if x != repeat_score]
+    remainder = [x for x in dice if x != repeat_score]
     return repeat_score * n + remainder
 
 def SumOfSingle(dice, selected):
@@ -24,18 +24,18 @@ def SmallStraight(dice):
     return 15 if tuple(sorted(dice)) == (1,2,3,4,5) else 0
 
 def LargeStraight(dice):
-    return 20 if tuple(sorted(dice)) == (2,3,4,5,6) else 0
+    return 30 if tuple(sorted(dice)) == (2,3,4,5,6) else 0
 
 def full_house(dice):
-    repeat_score = dice.highest_repeated(3)
+    repeat_score = dice.Highestrepeated(dice, 3)
     if repeat_score > 0:
-        rests = [x for x in dice.x if x != repeat_score]
+        rests = [x for x in dice if x != repeat_score]
         if len(set(rests)) == 1 and len(rests) == 2:
             return 25
         
-    repeat_score = dice.highest_repeated(2)
+    repeat_score = dice.Highestrepeated(dice, 2)
     if repeat_score > 0:
-        rests = [x for x in dice.x if x != repeat_score]
+        rests = [x for x in dice if x != repeat_score]
         if len(set(rests)) == 1 and len(rests) == 3:
             return 25
     return 0
