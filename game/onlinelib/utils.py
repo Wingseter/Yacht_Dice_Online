c977e55b78ae8b508925762100fe5b63a6e12abf
Wingseter <wingseter@gmail.com>
2020-10-04 23:10:06 +0900

    안내창 디스플레이 수정 및 마우스 클릭시 종료

--- game/onlinelib/utils.py
+++ game/onlinelib/utils.py
@@ -3,17 +3,19 @@ import pygame
 from loader import ONLINE
 
 def showLoading(win, errcode= -1):
-    pygame.draw.rect(win, (255, 255, 255), (200, 500, 600, 60))
-    pygame.draw.rect(win, (0,0,0), (203, 523, 694, 54))
+    pygame.draw.rect(win, (255, 255, 255), (300, 500, 600, 60))
+    pygame.draw.rect(win, (0,0,0), (303, 503, 594, 54))
 
     if errcode == -1:
-        win.blit(ONLINE.TRYCONN, (135, 240))
+        win.blit(ONLINE.TRYCONN, (303, 504))
         pygame.display.update()
         return 
     
-    win.blit(ONLINE.ERR[errcode], (100, 240))
+    win.blit(ONLINE.ERR[errcode], (303, 504))
     pygame.display.update()
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
+                return
+            if event.type == pygame.MOUSEBUTTONDOWN:
                 return
