66f48988f6c0939db88c9b257168a8944eaa5dfd
Jeonghwan Moon <bazzi2548@naver.com>
2020-11-10 18:22:08 +0900

    기능 구현 v2

--- item_testcode.py
+++ item_testcode.py
@@ -10,28 +10,51 @@ if event.type == pygame.MOUSEBUTTONDOWN:
         if dice_chance != 0:
             turn = turn -1
             dice_chance = dice_chance - 1
-    #홀 짝
+    #홀
     if 880 < x < 980 and 650 < y < 750:
         if n_select != 0:
 
              if 900 < x < 1100 and 500 < y < 600:
                     if turn < 3:
                         sound.play_roll(LOAD)
-                        score = roll(win, side, board, dicelist)
+                        score = odd_roll(win, side, board, dicelist)
                         diceAnimation(win, dices, dicelist.lenDice())
                         turn = turn + 1
 
             n_select = n_select - 1
-            
+    #짝        
     elif 1000 < x < 1100 and 650 < y < 750:
         if n_select != 0:
 
              if 900 < x < 1100 and 500 < y < 600:
                     if turn < 3:
                         sound.play_roll(LOAD)
-                        score = roll(win, side, board, dicelist)
+                        score = even_roll(win, side, board, dicelist)
                         diceAnimation(win, dices, dicelist.lenDice())
                         turn = turn + 1
 
             n_select = n_select - 1
                
+
+# core.py
+def odd_roll(win, side, board, dicelist):
+    dicelist.odd_roll_dice(win)
+    allDice = dicelist.giveAllDice()
+    score = calculate_score(allDice, board)
+
+def odd_roll_dice(self, win):
+    for i in range(len(self.__dice)):
+        self.__dice[i] = random.randint(ACE, SIXES, 2)
+
+    return score
+
+def even_roll(win, side, board, dicelist):
+    dicelist.even_roll_dice(win)
+    allDice = dicelist.giveAllDice()
+    score = calculate_score(allDice, board)
+
+def even_roll_dice(self, win):
+    for i in range(len(self.__dice)):
+        self.__dice[i] = random.randint(TWOS, SIXES, 2)
+
+    return score
