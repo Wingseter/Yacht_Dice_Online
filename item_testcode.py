8966ee99c4243b1eeb9f17352d2bc1d0a72c1e5d
Jeonghwan Moon <bazzi2548@naver.com>
2020-11-11 18:09:41 +0900

    함수 최적화

--- item_testcode.py
+++ item_testcode.py
@@ -2,6 +2,8 @@
 
 dice_chance = 1 #한번 더 굴리기 찬스 횟수
 n_select = 3 # 홀짝 가능 횟수
+item_num = 0 # 기본 = 0, 홀수 = 1, 짝수 = 2 배정 번호
+
 
 if event.type == pygame.MOUSEBUTTONDOWN:
     x,y event.pos
@@ -10,51 +12,47 @@ if event.type == pygame.MOUSEBUTTONDOWN:
         if dice_chance != 0:
             turn = turn -1
             dice_chance = dice_chance - 1
+    
     #홀
     if 880 < x < 980 and 650 < y < 750:
         if n_select != 0:
-
+            item_num = 1
              if 900 < x < 1100 and 500 < y < 600:
                     if turn < 3:
                         sound.play_roll(LOAD)
-                        score = odd_roll(win, side, board, dicelist)
+                        score = roll(win, side, board, dicelist, item_num)
                         diceAnimation(win, dices, dicelist.lenDice())
                         turn = turn + 1
-
             n_select = n_select - 1
+            item_num = 0
+    
     #짝        
     elif 1000 < x < 1100 and 650 < y < 750:
         if n_select != 0:
-
-             if 900 < x < 1100 and 500 < y < 600:
+            item_num = 2
+            if 900 < x < 1100 and 500 < y < 600:
                     if turn < 3:
                         sound.play_roll(LOAD)
-                        score = even_roll(win, side, board, dicelist)
+                        score = roll(win, side, board, dicelist, item_num)
                         diceAnimation(win, dices, dicelist.lenDice())
                         turn = turn + 1
-
             n_select = n_select - 1
+            item_num = 0
                
 
 # core.py
-def odd_roll(win, side, board, dicelist):
-    dicelist.odd_roll_dice(win)
-    allDice = dicelist.giveAllDice()
-    score = calculate_score(allDice, board)
+def roll(win, side, board, dicelist, item_num):
+        dicelist.roll_dice(win, item_num)
+        allDice = dicelist.giveAllDice()
+        score = calculate_score(allDice, board)
 
-def odd_roll_dice(self, win):
-    for i in range(len(self.__dice)):
-        self.__dice[i] = random.randint(ACE, SIXES, 2)
-
-    return score
-
-def even_roll(win, side, board, dicelist):
-    dicelist.even_roll_dice(win)
-    allDice = dicelist.giveAllDice()
-    score = calculate_score(allDice, board)
-
-def even_roll_dice(self, win):
-    for i in range(len(self.__dice)):
-        self.__dice[i] = random.randint(TWOS, SIXES, 2)
-
-    return score
+    def roll_dice(self, win, item_num): # 각 라운드 처음과 나머지 구분, 처음에는 dice, save 구분
+        if item_num == 0:
+            for i in range(len(self.__dice)):
+                self.__dice[i] = random.randint(ACE, SIXES)
+        elif item_num == 1:
+            for i in range(len(self.__dice)):
+                self.__dice[i] = random.randint(ACE, SIXES, 2)
+        elif item_num == 2:
+            for i in range(len(self.__dice)):
+                self.__dice[i] = random.randint(DEUCES, SIXES, 2)
